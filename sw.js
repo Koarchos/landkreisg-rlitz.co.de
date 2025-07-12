1e0c64b1e1568d6f839fc538f4f05e4650fb43a8
DeutscheBahn-Rbx <deutsche.bahn.rbx@gmail.com>
2025-07-12 19:39:53 +0200

    Merge GitHub main with local changes – Konflikte gelöst

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'Deutsche-Bahn-Roblox';
+const BASE = '/MBU.CO.DE/';  // Basis-Pfad für GitHub Pages
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => {
+    return cache.addAll([
+      BASE,
+      BASE + 'index.html',
+      BASE + 'styles/index/index.css',
+      BASE + 'pages/login.html',
+      BASE + 'styles/login/login.css',
+      BASE + 'pages/calender.html',
+      BASE + 'styles/kalender/calender.css',
+      BASE + 'install.js',
+      BASE + 'manifest.json',
+      BASE + 'icon512.png'
+    ]);
+  }));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.open(CACHE_NAME).then(cache =>
+    cache.match(event.request).then(resp => {
+      return resp || fetch(event.request).then(fresp => {
+        cache.put(event.request, fresp.clone());
+        return fresp;
+      });
+    })
+  ));
+});
